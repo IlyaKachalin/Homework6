22a978a611f969a041276e33e65760d4c57c0cda
Ilya <ilja.kachalin@yandex.ru>
2022-11-18 23:55:11 +0300

    tasks

---
+++ Task002/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+Console.Write("Введите число (b1). b1 = ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число (b2). b2 = ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число (k1). k1 = ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число (k2). k2 = ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+void FindPoint(double b1, double b2, double k2, double k1)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"{x}      {y}");
+}
+
+FindPoint(b1, b2, k2, k1);
---
+++ Tast001/Program.cs
@@ -0,0 +1,44 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+try
+{
+
+    Console.Write("Введите количество вводимых чисел (М). М = ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[m];
+
+    int[] Array(int m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            Console.Write("Введите число: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+
+        }
+        Console.WriteLine("Вводимый массив: [" + string.Join(", ", array) + "]");
+        return array;
+    }
+
+    int Count(int[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0) count++;
+        }
+        return count;
+    }
+
+    Array(m);
+    int result = Count(array);
+    Console.WriteLine($"Количество положительных чисел = {result}");
+}
+
+catch
+{
+    Console.WriteLine("Введите целое число");
+}
